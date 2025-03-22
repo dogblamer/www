1f136f2b7a9cbd27334712f2f0c49bd43be5c0e8
dogblamer <dogblamer@tuta.com>
2025-03-22 21:53:08 +0100

    fix: website deploys on cf

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import tailwind from "@astrojs/tailwind";
 export const siteUrl = "https://blamer.dog";
 
 const date = new Date().toISOString();
-// https://astro.build/config
 export default defineConfig({
     site: siteUrl + "/",
 
@@ -22,23 +21,6 @@ export default defineConfig({
                 item.priority = siteUrl + "/" === item.url ? 1.0 : 0.9;
                 item.changefreq = "weekly";
                 item.lastmod = date;
-
-                // if you want to exclude a page from the sitemap, do it here
-                // if (/exclude-from-sitemap/.test(item.url)) {
-                //     return undefined;
-                // }
-
-                // if any page needs a different priority, changefreq, or lastmod, uncomment the following lines and adjust as needed
-                // if (/test-sitemap/.test(item.url)) {
-                //     item.changefreq = "daily";
-                //     item.lastmod = date;
-                //     item.priority = 0.9;
-                // }
-
-                // if you want to change priority of all subpages like "/posts/*", you can use:
-                // if (/\/posts\//.test(item.url)) {
-                //     item.priority = 0.7;
-                // }
                 return item;
             },
         }),
@@ -47,12 +29,6 @@ export default defineConfig({
     prerender: true,
     vite: {
         plugins: [CompressionPlugin(), svgr()],
-        server: {
-            watch: {
-                usePolling: false,
-                interval: 1000,
-            },
-        },
     },
     buildOptions: {
         minify: true,
